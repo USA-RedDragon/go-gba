e508d7d6557c0a96e5ddec0e90fd123c2807d716
Jacob McSwain <jacob@mcswain.dev>
2023-06-17 14:46:08 -0500

    cpu: thumb: implement cmph

--- internal/emulator/cpu/isa/thumb/hi_register.go
+++ internal/emulator/cpu/isa/thumb/hi_register.go
@@ -24,9 +24,41 @@ type CMPH struct {
 func (c CMPH) Execute(cpu interfaces.CPU) (repipeline bool) {
 	fmt.Println("CMPH")
 
-	// This one needs to set condition flags
+	// Bits 7-6 are the hi operand flags
+	hof := c.instruction & (1<<7 | 1<<6) >> 6
+	// Bits 5-3 are the source register
+	rs := uint8(c.instruction & (1<<5 | 1<<4 | 1<<3) >> 3)
+	// Bits 2-0 are the destination register
+	rd := uint8(c.instruction & (1<<2 | 1<<1 | 1<<0))
 
-	panic("Not implemented")
+	switch hof {
+	case 0b01:
+		// cmp Rd, Hs
+		fmt.Printf("cmp r%d, r%d\n", rd, rs+8)
+		res := cpu.ReadRegister(rd) - cpu.ReadHighRegister(rs)
+		cpu.SetZ(res == 0)
+		cpu.SetN(res&0x80000000 != 0)
+		cpu.SetC(cpu.ReadRegister(rd) >= cpu.ReadHighRegister(rs))
+	case 0b10:
+		// cmp Hd, Rs
+		fmt.Printf("cmp r%d, r%d\n", rd+8, rs)
+		res := cpu.ReadHighRegister(rd) - cpu.ReadRegister(rs)
+		fmt.Printf("res: %08X\n", res)
+		cpu.SetZ(res == 0)
+		cpu.SetN(res&0x80000000 != 0)
+		cpu.SetC(cpu.ReadHighRegister(rd) >= cpu.ReadRegister(rs))
+	case 0b11:
+		// cmp Hd, Hs
+		fmt.Printf("cmp r%d, r%d\n", rd+8, rs+8)
+		res := cpu.ReadHighRegister(rd) - cpu.ReadHighRegister(rs)
+		fmt.Printf("res: %08X\n", res)
+		cpu.SetZ(res == 0)
+		cpu.SetN(res&0x80000000 != 0)
+		cpu.SetC(cpu.ReadHighRegister(rd) >= cpu.ReadHighRegister(rs))
+	default:
+		panic("Invalid hi operand flag")
+	}
+
 	return
 }
 
