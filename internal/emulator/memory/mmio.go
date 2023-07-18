fe13af0515b142c4fcae2b3561d21d8d42023c72
Jacob McSwain <jacob@mcswain.dev>
2023-07-18 00:35:04 -0500

    mmio: various fixes

--- internal/emulator/memory/mmio.go
+++ internal/emulator/memory/mmio.go
@@ -129,37 +129,40 @@ func (h *MMIO) mapMemory(addr uint32) uint32 {
 }
 
 // findMMIO finds the MMIO device index that contains the given address
-func (h *MMIO) findMMIOIndex(addr *uint32) (int, error) {
-	*addr = h.mapMemory(*addr)
+func (h *MMIO) findMMIOIndex(addr uint32) (int, error) {
+	addr = h.mapMemory(addr)
 
-	if *addr >= 0x0E000000 && *addr <= 0x0E00FFFF {
+	if addr >= 0x0E000000 && addr <= 0x0E00FFFF {
 		return 9, nil
-	} else if *addr >= 0x08000000 && *addr <= 0x09FFFFFF {
+	} else if addr >= 0x08000000 && addr <= 0x09FFFFFF {
 		return 8, nil
-	} else if *addr >= 0x07000000 && *addr <= 0x070003FF {
+	} else if addr >= 0x07000000 && addr <= 0x070003FF {
 		return 7, nil
-	} else if *addr >= 0x06000000 && *addr <= 0x06017FFF {
+	} else if addr >= 0x06000000 && addr <= 0x06017FFF {
 		return 6, nil
-	} else if *addr >= 0x05000000 && *addr <= 0x050003FF {
+	} else if addr >= 0x05000000 && addr <= 0x050003FF {
 		return 5, nil
-	} else if *addr >= 0x04000410 && *addr <= 0x04000411 {
+	} else if addr >= 0x04000410 && addr <= 0x04000411 {
 		return 4, nil
-	} else if *addr >= 0x04000000 && *addr <= 0x040003FE {
+	} else if addr >= 0x04000000 && addr <= 0x040003FE {
 		return 3, nil
-	} else if *addr >= 0x03000000 && *addr <= 0x03007FFF {
+	} else if addr >= 0x03000000 && addr <= 0x03007FFF {
 		return 2, nil
-	} else if *addr >= 0x02000000 && *addr <= 0x0203FFFF {
+	} else if addr >= 0x02000000 && addr <= 0x0203FFFF {
 		return 1, nil
-	} else if *addr < 0x00003FFF {
+	} else if addr < 0x00003FFF {
 		return 0, nil
 	}
 
-	return 0, fmt.Errorf("MMIO address %08x not found", *addr)
+	return 0, fmt.Errorf("MMIO address %08x not found", addr)
 }
 
 // Read8 reads a 8-bit value from the MMIO address space and returns it.
 func (h *MMIO) Read8(addr uint32) (uint8, error) {
-	index, err := h.findMMIOIndex(&addr)
+	if (addr >= 0x00004000 && addr <= 0x01FFFFFF) || (addr >= 0x10000000 && addr <= 0xFFFFFFFF) {
+		return 0, nil
+	}
+	index, err := h.findMMIOIndex(addr)
 	if err != nil {
 		return 0, err
 	}
@@ -172,11 +175,14 @@ func (h *MMIO) Read8(addr uint32) (uint8, error) {
 
 // Write8 writes a 8-bit value to the MMIO address space.
 func (h *MMIO) Write8(addr uint32, data uint8) error {
+	if (addr >= 0x00004000 && addr <= 0x01FFFFFF) || (addr >= 0x10000000 && addr <= 0xFFFFFFFF) {
+		return nil
+	}
 	if addr >= 0x06000000 && addr < 0x06FFFFFF {
 		// VRAM is not byte-addressable
 		return fmt.Errorf("VRAM address %08x not byte-addressable", addr)
 	}
-	index, err := h.findMMIOIndex(&addr)
+	index, err := h.findMMIOIndex(addr)
 	if err != nil {
 		return err
 	}
@@ -184,7 +190,6 @@ func (h *MMIO) Write8(addr uint32, data uint8) error {
 		fmt.Printf("MMIO write: 0x%08x 0x%02x\n", addr, data)
 	}
 	if !h.checkWritable(addr) {
-		panic("MMIO address not writable")
 		return fmt.Errorf("MMIO address %08x not writable", addr)
 	}
 	nonMapped := addr - h.mmios[index].address
@@ -197,7 +202,10 @@ func (h *MMIO) Write8(addr uint32, data uint8) error {
 
 // Read16 reads a 16-bit value from the MMIO address space and returns it.
 func (h *MMIO) Read16(addr uint32) (uint16, error) {
-	index, err := h.findMMIOIndex(&addr)
+	if (addr >= 0x00004000 && addr <= 0x01FFFFFF) || (addr >= 0x10000000 && addr <= 0xFFFFFFFF) {
+		return 0, nil
+	}
+	index, err := h.findMMIOIndex(addr)
 	if err != nil {
 		return 0, err
 	}
@@ -211,7 +219,10 @@ func (h *MMIO) Read16(addr uint32) (uint16, error) {
 
 // Write16 writes a 16-bit value to the MMIO address space.
 func (h *MMIO) Write16(addr uint32, data uint16) error {
-	index, err := h.findMMIOIndex(&addr)
+	if (addr >= 0x00004000 && addr <= 0x01FFFFFF) || (addr >= 0x10000000 && addr <= 0xFFFFFFFF) {
+		return nil
+	}
+	index, err := h.findMMIOIndex(addr)
 	if err != nil {
 		return err
 	}
@@ -233,7 +244,10 @@ func (h *MMIO) Write16(addr uint32, data uint16) error {
 
 // Read32 reads a 32-bit value from the MMIO address space and returns it.
 func (h *MMIO) Read32(addr uint32) (uint32, error) {
-	index, err := h.findMMIOIndex(&addr)
+	if (addr >= 0x00004000 && addr <= 0x01FFFFFF) || (addr >= 0x10000000 && addr <= 0xFFFFFFFF) {
+		return 0, nil
+	}
+	index, err := h.findMMIOIndex(addr & ^uint32(3))
 	if err != nil {
 		return 0, err
 	}
@@ -241,17 +255,28 @@ func (h *MMIO) Read32(addr uint32) (uint32, error) {
 		mod := addr % 0x8000
 		addr = 0x03000000 + mod
 	}
-	nonMapped := addr - h.mmios[index].address
+	nonMapped := (addr & ^uint32(3)) - h.mmios[index].address
 	if nonMapped >= h.mmios[index].size {
 		return 0, fmt.Errorf("MMIO address %08x not found", addr)
 	}
 	dataBytes := h.mmios[index].data[nonMapped : nonMapped+4]
-	return uint32(dataBytes[0]) | uint32(dataBytes[1])<<8 | uint32(dataBytes[2])<<16 | uint32(dataBytes[3])<<24, nil
+	val := uint32(dataBytes[0]) | uint32(dataBytes[1])<<8 | uint32(dataBytes[2])<<16 | uint32(dataBytes[3])<<24
+	if addr&3 > 0 { // https://github.com/jsmolka/gba-tests/blob/a6447c5404c8fc2898ddc51f438271f832083b7e/thumb/memory.asm#L72
+		is := 8 * (uint(addr) & 3)
+		is %= 32
+		tmp0 := (val) >> (is)
+		tmp1 := (val) << (32 - (is))
+		return tmp0 | tmp1, nil
+	}
+	return val, nil
 }
 
 // Write32 writes a 32-bit value to the MMIO address space.
 func (h *MMIO) Write32(addr uint32, data uint32) error {
-	index, err := h.findMMIOIndex(&addr)
+	if (addr >= 0x00004000 && addr <= 0x01FFFFFF) || (addr >= 0x10000000 && addr <= 0xFFFFFFFF) {
+		return nil
+	}
+	index, err := h.findMMIOIndex(addr)
 	if err != nil {
 		return err
 	}
@@ -263,7 +288,6 @@ func (h *MMIO) Write32(addr uint32, data uint32) error {
 		fmt.Printf("MMIO write: 0x%08x 0x%08x\n", addr, data)
 	}
 	if !h.checkWritable(addr) {
-		panic("MMIO address not writable")
 		return fmt.Errorf("MMIO address %08x not writable", addr)
 	}
 	nonMapped := addr - h.mmios[index].address
